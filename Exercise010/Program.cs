aed3c11d31405d739ead70778620c79d59730850
Anton <antonkononoff@yandex.ru>
2022-11-02 20:57:53 +0500

    Added first Ex

---
+++ Exercise010/Program.cs
@@ -0,0 +1,11 @@
+﻿void SecondDigit(int x)
+{
+    int n1 = x % 100;
+    double result = n1 / 10;
+    Console.WriteLine("Вторая цифра числа - " + result);
+}
+
+int input = 379; 
+
+SecondDigit(input);
+
